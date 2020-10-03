68b2b7b791c706952e3edac6be60f811c59ad601
Daniel Reis <dreis@opensourceintegrators.com>
2020-10-03 18:47:59 +0100

    wip

---
+++ l10n_pt_partner/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2020 - Daniel Reis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Portugal specific Partner master data",
+    "version": "12.0.0.0.1",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "author": "Daniel Reis, Odoo Community Association (OCA)",
+    "summary": "Portugal specific Partner master data",
+    "website": "https://github.com/OCA/l10n-portugal",
+    "category": "Localisation/Portugal",
+    "data": [],
+    "installable": True,
+}
